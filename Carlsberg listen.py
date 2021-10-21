da24c5a491d560831b02a28d605014911d67677d
NBentsen <92266717+NBentsen@users.noreply.github.com>
2021-10-21 16:00:44 +0200

    Add files via upload
    Besvarelse til øvelse 1.1-1.4 - øvelse "Funktioner & lister"

---
+++ Carlsberg listen.py
@@ -0,0 +1,14 @@
+thislist = ["Carlsberg", "Tuborg", "Albani"]
+thislist.append("Faxe")
+print(thislist)
+
+thislist = ["Carlsberg", "Tuborg", "Albani", "Faxe"]
+print(thislist[0])
+
+thislist[3] = Gulddame
+print(thislist)
+
+thislist = ["Carlsberg", "Carlsberg", "Carlsberg", "Tuborg", "Albani", "Faxe"]
+
+
+list = ["Hvad hedder verdens fattigste konge? - Kong Kurs", "Joke2", "Joke3"
