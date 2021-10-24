7fdd6666821c82ee59a93917f6c257f0be9b16bc
NBentsen <92266717+NBentsen@users.noreply.github.com>
2021-10-24 13:46:51 +0200

    Main der kalder funktioner fra "modul_1"

---
+++ Funktioner og lister til import/main.py
@@ -0,0 +1,12 @@
+import modul_1
+
+import time
+
+
+print(modul_1.func1("Importeret ", "fra ", "module_1"))
+
+modul_1.func2()
+
+print(modul_1.func3(10, 10, 10))
+
+print(modul_1.sixArgs("hej", "det virker", 1, 2))
---
+++ Funktioner og lister til import/modul_1.py
@@ -0,0 +1,12 @@
+def func1(arg1, arg2, arg3):
+    return arg1 + arg2 + arg3
+print(func1(10, 10, 10))
+
+def func2():
+    print("Printer ting og sager")
+    
+def func3(arg1, arg2, arg3):
+    return arg1 * arg2 * arg3
+
+def sixArgs(arg1 : str, arg2 : str, arg3: int, arg4 : int, arg5 = 1, arg6 = 0):
+    return arg1, arg2, arg3, arg4, arg5, arg6
